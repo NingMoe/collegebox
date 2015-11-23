54f82274e68afa0a3b84c8d4df95879facf210ab
tiancaizjzk <xiaoyaopalading@sina.com>
2015-11-24 00:04:48 +0800

    .

--- webservice/src/main/java/com/collegebox/webservice/service/invitationcode/impl/InvitationCodeServiceImpl.java
+++ webservice/src/main/java/com/collegebox/webservice/service/invitationcode/impl/InvitationCodeServiceImpl.java
@@ -5,8 +5,21 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.collegebox.webservice.pojo.InvitationCode;
+import com.collegebox.webservice.service.invitationcode.InvitationCodeService;
 
 @Service
-public class InvitationCodeServiceImpl {
+public class InvitationCodeServiceImpl implements InvitationCodeService {
+
+	@Override
+	public List<InvitationCode> gererateInvitaitonCode(Integer counts, Long agencyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inviteStudent(Long id, Long studentId) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
